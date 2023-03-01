0f7a024b8d4229bfa3297b742dc9b05e98802137
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2023-03-01 11:02:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT | 3000;
+const port = process.env.PORT;
 
 const vendorController = require('./controllers/vendorController');
 const transactionController = require('./controllers/transactionController');
